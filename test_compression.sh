a85d6297ea4b21e090a33a6f2ea8b3b4558a2664
melkherj <melkherj@gmail.com>
2014-01-13 18:28:22 -0800

    Added space error analysis to git repo

--- test_compression.sh
+++ test_compression.sh
@@ -4,7 +4,7 @@ wd=`pwd`
 cd $SMART_GRID_SRC
 
 #~/part-00000
-cat /home/melkherj/SmartGridProject/src/part-00099 | ./model_power_data/compress_all_tags/reduce.py > "${data_dir}/out.txt"
+cat /home/melkherj/SmartGridProject/data/part-00099 | ./model_power_data/compress_all_tags/reduce.py > "${data_dir}/out.txt"
 ./evaluate_visualize_model/process_date_time_errors.py "${data_dir}/out.txt"
 
 cd $wd
