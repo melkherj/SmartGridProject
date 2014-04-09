59c706b08da80e63a4ae461163b45be33971bfa6
melkherj <melkherj@gmail.com>
2014-04-09 11:35:52 -0700

    Modified post-processing of compressed data, now just gets errors

--- evaluate_visualize_model/process_just_errors.py
+++ evaluate_visualize_model/process_just_errors.py
@@ -20,33 +20,26 @@ from collections import defaultdict
 '''
 
 def hash_line(line, predictor_hash, tag_hash, date_hash):
-    predictor, tag, date, compressed, space, error, quantile_lower, \
-        quantile_upper = line.split('^')
+    predictor, tag, date, _, space, error, _, \
+        _ = line.split('^')
     predictor = predictor_hash[predictor]
     tag = tag_hash[tag]
     date = date_hash[date]
     space = float(space)
     error = float(error)
-    quantile_lower = float(quantile_lower)
-    quantile_upper  = float(quantile_upper)
-    return (predictor, tag, date, compressed, space, error, quantile_lower, 
-        quantile_upper)
+    return (predictor, tag, date, space, error)
 
 def load_compressed_df(filename, save=False):
     ''' Load the compressed representation of sensor data in a dataframe '''
+    print 'Creating date/tag/compression method vocabulary'
     # Create vocabularies for tags, dates, and predictors.  Count number of lines
     tag_set = set([])
     date_set = set([])
     predictor_set = set([])
-    aggregates = defaultdict(dict)
     with open(filename, 'r') as f:
         for i,line in enumerate(f): #f is a lazy generator of lines
             if line[:3] == '^^^':
-                predictor, tag, aggregate = line[3:].split('^')
-                aggregates[predictor][tag] = aggregate
                 continue
-            if i % 100000 == 0:
-                print i
             predictor, tag, date, _,  _, _, _, _ = line.split('^')
             predictor_set.add(predictor)
             tag_set.add(tag)
@@ -61,24 +54,19 @@ def load_compressed_df(filename, save=False):
     date_hash = dict((date, i) for i,date in enumerate(date_list))
 
     with open(filename, 'r') as f:
+        print 'Creating list of tuples'
         E = [hash_line(line, predictor_hash, tag_hash, date_hash) for line in f if not line[:3] == '^^^']
-        predictors, tags, dates, all_compressed, spaces, errors, quantile_lower, \
-            quantile_upper = zip(*E)
+        print 'Creating DataFrame'
+        predictors, tags, dates, spaces, errors = zip(*E)
         predictors = pd.Series(predictors, dtype=np.int32, name='predictor') 
         tags = pd.Series(tags, dtype=np.int32, name='tag') 
         dates = pd.Series(dates, dtype=np.int32, name='date') 
-        all_compressed = pd.Series(all_compressed, dtype=np.object, 
-            name='compressed') 
         spaces = pd.Series(spaces, dtype=np.float32, name='space') 
         errors = pd.Series(errors, dtype=np.float32, name='error') 
-        quantile_lower = pd.Series(quantile_lower, 
-            dtype=np.float32, name='quantile_lower') 
-        quantile_upper = pd.Series(quantile_upper, 
-            dtype=np.float32, name='quantile_upper') 
 
     # Convert to DataFrame 
-    df = pd.concat([predictors, tags, dates, all_compressed, spaces, errors, 
-        quantile_lower, quantile_upper], axis=1)
+    df = pd.concat([predictors, tags, dates, spaces, errors], axis=1)
+    print 'Reindexing DataFrame'
     df = df.set_index(['tag','date','predictor']).unstack(level=-1)
     context = {
              'predictor_list':predictor_list, 
@@ -86,8 +74,7 @@ def load_compressed_df(filename, save=False):
              'tag_list':tag_list, 
              'tag_hash':tag_hash, 
              'date_list':date_list,
-             'date_hash':date_hash,
-             'aggregates':aggregates
+             'date_hash':date_hash
     }
     if save:
         # Save DataFrame/vocabulary lists to files
